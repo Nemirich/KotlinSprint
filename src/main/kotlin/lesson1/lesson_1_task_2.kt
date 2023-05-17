79d57ab547943277913e7ac4e92e20968337a185
gleb_ <nemirichgleb@gmail.com>
2023-05-17 20:49:23 +0300

    KS-1-2 print data from second lesson

---
+++ src/main/kotlin/lesson1/lesson_1_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main () {
+
+    val ordersNumber:Int = 75
+    val thankYouSoMuch:String = "Премногоблагодарны вам за ваши деньги"
+
+    println(ordersNumber)
+    println(thankYouSoMuch)
+
+    var numberOfEmployees:Int = 2000
+    // println(numberOfEmployees)
+
+    numberOfEmployees = 2000 - 1
+    println(numberOfEmployees)
+
+}
+
+

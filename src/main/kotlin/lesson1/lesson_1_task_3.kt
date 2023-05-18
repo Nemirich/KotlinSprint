bb6db5edfe08336e5af2693929dab52313df9f62
gleb_ <nemirichgleb@gmail.com>
2023-05-18 18:31:41 +0300

    KS-1-3 print data from third lesson (UPD)

--- src/main/kotlin/lesson1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson1/lesson_1_task_3.kt
@@ -1,20 +1,22 @@
 package lesson1
 
-fun main () {
+fun main() {
 
     val year = 1961
-    var hour:Int = 9
-    var minute:Int = 7
+    var hour: Int = 9
+    var minute: Int = 7
 
-    println("""
-        $year
-        $hour
-        $minute
-    """.trimIndent())
+    val n = 2
+
+    println("$year")
+    println(hour.toString().padStart(n, '0'))
+    println(minute.toString().padStart(n, '0'))
 
     hour = 10
     minute = 55
 
-    println("$hour : $minute")
+    val clockSign: Char = ':'
+
+    println("$hour$clockSign$minute")
 
 }

75ce98a4b10489e684f2857b8b400f468302898e
gleb_ <nemirichgleb@gmail.com>
2023-05-23 13:54:01 +0300

    KS-1-5 print data from 5-th lesson (UPD)

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -8,23 +8,24 @@ import java.time.format.DateTimeFormatter
 import kotlin.time.DurationUnit
 import kotlin.time.TimeSource
 
-fun main () {
+fun main() {
 
     val seconds = 6480
     // кол-во секунд, которое гагарин пробыл в космосе
 
     val secondsInMinute = 60
-
     val ostatok = seconds%secondsInMinute // проверяем, что делится без остатка
     //println("$ostatok")
 
-    //println(seconds/secondsInMinute)
+    val minutes: Int = (seconds/secondsInMinute)
+    println(minutes)
+    // кол-во минут, которое гагарин пробыл в космосе
 
-    val minutes = seconds/secondsInMinute
+    val inValue = 0
+    val n = 2
+    val secondInTime = inValue.toString().padStart(n, '0')
+    println(secondInTime)
+    // кол-во секунд которое гагарин пробыл в космосе в формате "00"
 
-    val secondsInSpace1 = 0
-    val secondsInSpace2 = 0
-
-    println("$minutes : $secondsInSpace1$secondsInSpace2")
+    println("$minutes:$secondInTime")
 }
-

9bf7875fd38b91514d7e2e550cc3f1469d953f01
gleb_ <nemirichgleb@gmail.com>
2023-05-23 23:31:05 +0300

    KS-1-5 print data from 5-th lesson (UPD)

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -14,10 +14,10 @@ fun main() {
     // кол-во секунд, которое гагарин пробыл в космосе
 
     val secondsInMinute = 60
-    val ostatok = seconds%secondsInMinute // проверяем, что делится без остатка
+    val ostatok = seconds % secondsInMinute // проверяем, что делится без остатка
     //println("$ostatok")
 
-    val minutes: Int = (seconds/secondsInMinute)
+    val minutes: Int = (seconds / secondsInMinute)
     println(minutes)
     // кол-во минут, которое гагарин пробыл в космосе
 
@@ -27,5 +27,7 @@ fun main() {
     println(secondInTime)
     // кол-во секунд которое гагарин пробыл в космосе в формате "00"
 
-    println("$minutes:$secondInTime")
+    val dvoetochie: Char = ':'
+
+    println("$minutes$dvoetochie$secondInTime")
 }

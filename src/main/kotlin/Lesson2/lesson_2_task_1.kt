939aefd5ffca8df556c35d775bd531c1d51320bc
gleb_ <nemirichgleb@gmail.com>
2023-05-24 13:32:09 +0300

    KS-2-1 print data from first lesson

---
+++ src/main/kotlin/Lesson2/lesson_2_task_1.kt
@@ -0,0 +1,14 @@
+package Lesson2
+
+fun main() {
+
+    val SQuantity = 4f
+
+    val SOne = 3
+    val STwo = 4
+    val SThree = 3
+    val SFour = 5
+
+    println((SOne + STwo + SThree + SFour) / SQuantity)
+    // Написать выражение, которое вычисляет средний балл
+}

fc34fc8fbed6ceceb5f9e2ffa4e6f73801fc3d52
gleb_ <nemirichgleb@gmail.com>
2023-05-25 14:53:09 +0300

    KS-2-4 print data from 4-th lesson

--- src/main/kotlin/lesson2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson2/lesson_2_task_4.kt
@@ -2,12 +2,17 @@ package lesson2
 fun main() {
 
     val crystal = 7 // добыто единиц кристалической руды
+    val crystalName: String = "Кристаллическая руда"
+
     val iron = 11 // добыто единиц металлической руды
+    val ironName: String = "Металлическая руда"
+
     val bonus = 5 // {20% = 1/5} => (x * (1/5) = x / 5)
 
     val crystalBonus = crystal / bonus
     val ironBonus = iron / bonus
 
-    println(crystalBonus)
-    println(ironBonus)
+    println("$crystalBonus $crystalName")
+    println("$ironBonus $ironName")
+
 }

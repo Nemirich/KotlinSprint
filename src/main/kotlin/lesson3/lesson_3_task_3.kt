0de3c7ac39d2b28161a7325189e06ea26b710c44
gleb_ <nemirichgleb@gmail.com>
2023-05-25 19:56:07 +0300

    KS-3-3 print data from trird lesson

---
+++ src/main/kotlin/lesson3/lesson_3_task_3.kt
@@ -0,0 +1,11 @@
+package lesson3
+
+fun main() {
+
+    val number = 9 // задаваемое число
+    for (i in 1..9) {
+        val result = number * i
+        println("$number * $i = $result")
+    }
+
+}
